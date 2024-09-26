3641535dee2f457b3c5f02caf886e0d822568d1a
Irga <irgantaraa@gmail.com>
2024-09-26 21:25:20 +0700

    first commit

---
+++ src/main/java/com/juaracoding/JavaFirst.java
@@ -0,0 +1,18 @@
+package com.juaracoding;
+
+public class JavaFirst {
+    public static void main(String[] args) {
+        int x = 100;
+        System.out.println("Hello Dunia");
+        System.out.println("Indonesia");
+
+        if( true ){
+            System.out.println("True");
+            System.out.println(x);
+            }
+        for ( int i = 0; i < 10; i++) {
+            System.out.println(i);
+            System.out.println(x);
+        }
+    }
+}
